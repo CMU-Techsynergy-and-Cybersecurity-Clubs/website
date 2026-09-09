67e5aece7a6a7f1a824f3c59ae563a7b7472dca7
Jayden Alonzo-Estrada <142930712+Jaynuke79@users.noreply.github.com>
2026-09-09 16:02:42 -0600

    Update meeting time and location to Confluence Hall 315 at 3 PM

--- src/lib/sharedConfig.ts
+++ src/lib/sharedConfig.ts
@@ -3,5 +3,5 @@ import type { ClubConfig } from '@/lib/types'
 export const sharedClubConfig: ClubConfig = {
   email: 'jjalonzo-estra@mavs.coloradomesa.edu',
   discord: 'https://discord.gg/Uyy3Baqxnb',
-  meeting: { day: 'Wednesdays', time: '4:00 PM', location: 'Confluence 110' },
+  meeting: { day: 'Wednesdays', time: '3:00 PM', location: 'Confluence Hall 315' },
 }
